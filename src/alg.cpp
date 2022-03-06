ab11580a19c87abde990c8d2a9a896cdc780f288
Матвеев Артем <99340742+MATVEEVru@users.noreply.github.com>
2022-03-06 23:52:01 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,60 +4,45 @@
 
 
 double pown(double value, uint16_t n) {
-  if (n == 1) {
-    return value;
-  } else if (n == 0) {
-    return 1;
-  }
-  return value * pown(value, --n);
+if (n == 1) {
+return value;
+} else if (n == 0) {
+return 1;
+}
+return value * pown(value, --n);
 }
 
 uint64_t fact(uint16_t n) {
-   if (n <= 1) {
-    return 1;
-  }
-  return n * fact(n - 1);
+if (n <= 1) {
+return 1;
+}
+return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
- return pown(x, n) / fact(n);
+return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
 double sum = 0;
-  for (uint16_t i = 0, n = 0; i <= count; ++i, ++n) {
-    sum += calcItem(x, n);
-  }
-  return sum;
+for (uint16_t i = 0, n = 0; i <= count; ++i, ++n) {
+sum += calcItem(x, n);
+}
+return sum;
 }
 
 double sinn(double x, uint16_t count) {
-long double sin = 0;
-    int n = 1;
-    for (uint64_t i = 1; i <= count; i++) {
-        if (i % 2 != 0) {
-            sin = sin + (pown(x, n) / fact(n));
-        }
-        else {
-            sin = sin - (pown(x, n) / fact(n);
-        }
-        n = n + 2;
-    }
+double sum = 0;
+for (uint16_t i = 1, n = 1; i <= count; ++i, ++n) {
+sum += pown(-1, n - 1) * calcItem(x, 2 * n - 1);
+}
+return sum;
 }
 
 double cosn(double x, uint16_t count) {
-double cos = 1;
-    int n = 2;
-    for (uint64_t i = 1; i <= count; i++) {
-        for (uint64_t i = 1; i < count; i++) {
-            if (i % 2 != 0) {
-                cos = cos - (pown(x, n) / fact(n));
-            }
-            else {
-                cos = cos + (pown(x, n) / fact(n));
-            }
-            n = n + 2;
-        }
-        return cos;
-    }
+double sum = 0;
+for (uint16_t i = 1, n = 1; i <= count; ++i, ++n) {
+sum += pown(-1, n - 1) * calcItem(x, 2 * n - 2);
+}
+return sum;
 }
